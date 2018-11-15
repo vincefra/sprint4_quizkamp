78fcbb290543cd821f0370618c564acc28065383
Vincent <vincent.frank@yh.nackademin.se>
2018-11-15 11:28:37 +0100

    Cleaned up

--- src/Sprint4_quizkamp_server/TcpServer.java
+++ src/Sprint4_quizkamp_server/TcpServer.java
@@ -14,38 +14,36 @@ import java.net.Socket;
  *
  * @author xingao
  */
-public class TcpServer implements Runnable{
-   private Socket clientSocket;
-   Thread activity= new Thread(this);
+public class TcpServer implements Runnable
+{
+    private Socket clientSocket;
+    private Thread activity = new Thread(this);
     
-     public TcpServer(Socket clientSocket) throws IOException {
+    public TcpServer(Socket clientSocket) throws IOException 
+    {
         this.clientSocket = clientSocket;
-        activity.start();
-
-        
+        activity.start();   
     }
-    
-    
-    public void run(){
 
-        try ( 
-            
+    public void run()
+    {
+        try( 
                 PrintWriter sendToClient =
                 new PrintWriter(clientSocket.getOutputStream(), true);
             
                 BufferedReader dataIncoming = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            String inputLine;
-            
-           
+            ) 
+        {
             sendToClient.println("Hello");
-            
-            /*while ((inputLine = dataIncoming.readLine()) != null) {           
+
+            String inputLine;
+            while ((inputLine = dataIncoming.readLine()) != null)         
                sendToClient.println("");
-            }*/
+            
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     } 
