eb325e4b66e24ba16469ee8575b21cda60d742dd
William Jelvin <33911562+WJelvin@users.noreply.github.com>
2018-11-15 13:13:16 +0100

    cleaned up some code, added a few comments

--- src/Sprint4_quizkamp_server/Client/Client.java
+++ src/Sprint4_quizkamp_server/Client/Client.java
@@ -1,34 +1,48 @@
 package Sprint4_quizkamp_server.Client;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class Client {
 
     public Client() {
 
+        //Create ip/port-variables and an arraylist for storing received objects
+        ArrayList<Object> myReceivedObjects = new ArrayList<>();
         int port = 12345;
-        String ip = "172.20.201.231";
+        InetAddress ip = null;
+        try {
+            ip = InetAddress.getByName("172.20.201.168");
+        } catch (UnknownHostException e) {
+            System.out.println("InetAdress - IP address of host could not be determined");
+            e.printStackTrace();
+        }
 
-        try (Socket clientSocket = new Socket(ip, port);
-             ObjectOutputStream objectOut = new ObjectOutputStream(clientSocket.getOutputStream());
-             ObjectInputStream objectIn = new ObjectInputStream(clientSocket.getInputStream())) {
-
-            //BufferedReader readerIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            //String receivedString;
+        //Try connecting to server
+        try (Socket socketToServer = new Socket(ip, port);
+             ObjectOutputStream objectOut = new ObjectOutputStream(socketToServer.getOutputStream());
+             ObjectInputStream objectIn = new ObjectInputStream(socketToServer.getInputStream())) {
 
             Object receivedObject;
 
+            //Send initialization object when connected
+            objectOut.writeObject("initObject");
+
+            //when receiving an object, add it to an arraylist and send another object to the server
             while ((receivedObject = objectIn.readObject()) != null) {
-                try {
-                    System.out.println(receivedObject);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                    myReceivedObjects.add(receivedObject);
+                    objectOut.writeObject("bye");
             }
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("Kunde inte ansluta.");
             e.printStackTrace();
+        }
+        //Print all objects from list of received objects
+        for (Object o : myReceivedObjects) {
+            System.out.println("Client: Received object: " + o);
         }
     }
 }
