20ec1f879ede0463b72f008f502bbb8cb8e0d4dc
Xin Gao <Xin.Gao@yh.nackademin.se>
2018-11-15 10:39:31 +0100

    test sträng funkar

---
+++ src/Sprint4_quizkamp_server/TcpListener.java
@@ -0,0 +1,30 @@
+/*
+ *  
+Java18-OOJ
+ */
+package Sprint4_quizkamp_server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author xingao
+ */
+public class TcpListener {
+    public static void main(String[] args) throws IOException {
+    ServerSocket serverSocket = new ServerSocket(12345);
+        while (true) {
+            try {
+                final Socket socketToClient = serverSocket.accept();
+                TcpServer clientHandler = 
+                        new TcpServer(socketToClient);
+                
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } 
+    }
+}
---
+++ src/Sprint4_quizkamp_server/TcpServer.java
@@ -0,0 +1,52 @@
+/*
+ *  
+Java18-OOJ
+ */
+package Sprint4_quizkamp_server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author xingao
+ */
+public class TcpServer implements Runnable{
+   private Socket clientSocket;
+   Thread activity= new Thread(this);
+    
+     public TcpServer(Socket clientSocket) throws IOException {
+        this.clientSocket = clientSocket;
+        activity.start();
+
+        
+    }
+    
+    
+    public void run(){
+
+        try ( 
+            
+                PrintWriter sendToClient =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            
+                BufferedReader dataIncoming = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            
+           
+            sendToClient.println("Hello");
+            
+            /*while ((inputLine = dataIncoming.readLine()) != null) {           
+               sendToClient.println("");
+            }*/
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    } 
+}
