d5f2b7a07ecee424cdf89bef40f4053416f1e9d7
Vincent <vincent.frank@yh.nackademin.se>
2018-11-15 13:24:06 +0100

    More clean ups

--- src/Sprint4_quizkamp_server/Server/TcpListener.java
+++ src/Sprint4_quizkamp_server/Server/TcpListener.java
@@ -13,16 +13,19 @@ import java.net.Socket;
  * @author xingao
  */
 public class TcpListener {
-    public static void main(String[] args) throws IOException {
-    ServerSocket serverSocket = new ServerSocket(12345);
-        while (true) {
-            try {
+    public static void main(String[] args) throws IOException 
+    { 
+        ServerSocket serverSocket = new ServerSocket(12345);
+    
+        while (true) 
+        {
+            try 
+            {
                 final Socket socketToClient = serverSocket.accept();
-                TcpServer clientHandler = 
-                        new TcpServer(socketToClient);
-                
-
-            } catch (IOException e) {
+                TcpServer clientHandler = new TcpServer(socketToClient);
+            } 
+            catch (IOException e) 
+            {
                 e.printStackTrace();
             }
         } 
--- src/Sprint4_quizkamp_server/Server/TcpServer.java
+++ src/Sprint4_quizkamp_server/Server/TcpServer.java
@@ -4,48 +4,37 @@ Java18-OOJ
  */
 package Sprint4_quizkamp_server.Server;
 
-import java.awt.Image;
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
-import javax.imageio.ImageIO;
-import javax.swing.Icon;
 
 /**
  *
  * @author xingao
  */
-public class TcpServer implements Runnable{
-   private Socket clientSocket;
-   Thread activity= new Thread(this);
+public class TcpServer implements Runnable
+{
+    private Socket clientSocket;
+    private Thread activity = new Thread(this);
     
-     public TcpServer(Socket clientSocket) throws IOException {
+    public TcpServer(Socket clientSocket) throws IOException 
+    {
         this.clientSocket = clientSocket;
-        activity.start();
-
-        
+        activity.start();    
     }
-    
-    
+
+   @Override
     public void run(){
 
         try ( 
-           
-            ObjectOutputStream oos = new ObjectOutputStream(
-                    clientSocket.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(
-                    clientSocket.getInputStream());
-        ) {           
+            ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+        ) 
+        {          
             String inputLine;
-           
-            
-            
-            while ((inputLine = (String)ois.readObject()) != null) {   
+            while ((inputLine = (String)ois.readObject()) != null) 
+            {   
                if(inputLine.equalsIgnoreCase("Hej"))
                    oos.writeObject("Hello");
                else if (inputLine.equalsIgnoreCase("Bye"))
@@ -53,12 +42,11 @@ public class TcpServer implements Runnable{
                   oos.writeObject("Hej då");
                   break;
                }
-                   
-               
                System.out.print(inputLine);
             }
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     } 
