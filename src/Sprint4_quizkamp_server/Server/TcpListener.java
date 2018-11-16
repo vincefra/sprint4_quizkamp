9612ced22ab707b7cc6847e7500b1e34b3a94018
Xin Gao <Xin.Gao@yh.nackademin.se>
2018-11-16 11:22:00 +0100

    Fixed multiuser

--- src/Sprint4_quizkamp_server/Server/TcpListener.java
+++ src/Sprint4_quizkamp_server/Server/TcpListener.java
@@ -16,13 +16,23 @@ public class TcpListener {
     public static void main(String[] args) throws IOException 
     { 
         ServerSocket serverSocket = new ServerSocket(12345);
-    
+        System.out.println("Quizkampen is running");
+        
         while (true) 
         {
             try 
             {
-                final Socket socketToClient = serverSocket.accept();
-                TcpServer clientHandler = new TcpServer(socketToClient);
+                //final Socket socketToClient = serverSocket.accept();
+                TcpServer playerX = new TcpServer(serverSocket.accept(),'X');
+                System.out.println("p1 connected");
+                
+                TcpServer playerO = new TcpServer(serverSocket.accept(),'O');
+                System.out.println("p2 connected");
+
+                //playerX.setOpponent(playerO);
+                //playerO.setOpponent(playerX);
+                
+//                game.currentPlayer = playerX;
             } 
             catch (IOException e) 
             {
--- src/Sprint4_quizkamp_server/Server/TcpServer.java
+++ src/Sprint4_quizkamp_server/Server/TcpServer.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,37 +19,61 @@ public class TcpServer implements Runnable
 {
     private Socket clientSocket;
     private Thread activity = new Thread(this);
+    TcpServer opponent;
+    char mark;
+    ObjectOutputStream oos;
+    ObjectInputStream ois;
     
-    public TcpServer(Socket clientSocket) throws IOException 
+    public TcpServer(Socket clientSocket,char mark) throws IOException 
     {
         this.clientSocket = clientSocket;
-        activity.start();    
+        this.mark= mark;
+        activity.start(); 
+//        try { 
+//            oos = new ObjectOutputStream(clientSocket.getOutputStream());
+//            ois = new ObjectInputStream(clientSocket.getInputStream());
+//       
+//            oos.writeObject("Welcome");
+////            oos.writeObject("Welcome");
+//
+//        }    
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
     }
 
+    
+     public void setOpponent(TcpServer opponent) {
+            this.opponent = opponent;
+        }
+     
    @Override
     public void run(){
 
-        try ( 
-            ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
-        ) 
-        {          
-            String inputLine;
-            while ((inputLine = (String)ois.readObject()) != null) 
-            {   
-               if(inputLine.equalsIgnoreCase("Hej"))
-                   oos.writeObject("Hello");
-               else if (inputLine.equalsIgnoreCase("Bye"))
-               {
-                  oos.writeObject("Hej då");
-                  break;
-               }
-               System.out.print(inputLine);
-            }
-        }
-        catch (Exception e)
+        while (true)
         {
-            e.printStackTrace();
+            
         }
+//        try {
+//            oos.writeObject("All players connected");
+////            if(mark=='X')
+////                oos.writeObject("Start a new game?");
+//        
+//            
+//            ServerProtocol protocol= new ServerProtocol();
+//            String inputLine;
+//            
+//            oos.writeObject(protocol.processInput(null));
+//            
+//            while ((inputLine = (String) ois.readObject()) != null) {          
+//               oos.writeObject(protocol.processInput(inputLine));
+//            }
+//       } catch (IOException ex) {
+//            Logger.getLogger(TcpServer.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(TcpServer.class.getName()).log(Level.SEVERE, null, ex);
+//        
+//        }
     } 
 }
