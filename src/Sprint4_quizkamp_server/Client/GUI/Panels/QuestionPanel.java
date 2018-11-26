264cbabbf913be92da573b00064d095d34513ae2
Xin Gao <Xin.Gao@yh.nackademin.se>
2018-11-26 11:03:28 +0100

    Gui

--- src/Sprint4_quizkamp_server/Client/GUI/Panels/QuestionPanel.java
+++ src/Sprint4_quizkamp_server/Client/GUI/Panels/QuestionPanel.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import jdk.nashorn.api.tree.BreakTree;
+
 
 public class QuestionPanel extends JPanel implements ActionListener {
 
@@ -19,36 +19,46 @@ public class QuestionPanel extends JPanel implements ActionListener {
     private JButton answer4Button = new JButton("Svar 4");
     private ShowQuestionAction action;
     private String[] answers;
-
+    private Color color= new Color(96,165,191);
+    
+    private Font font= new Font("SansSerif", Font.PLAIN, 28);
+    private Icon backIcon= new ImageIcon("src/background.jpg");
+    private Dimension dimenstion= new Dimension(160,50);
+    
+    
     public QuestionPanel(ShowQuestionAction action) {
         this.action = action;
         this.answers = action.question.getAnswers(true);
 
         setBackground(new Color(0, 0, 55));
         setLayout(new BorderLayout());
+        questionLabel.setIcon(backIcon);
         questionLabel.setText(action.question.getQuestion());
+
         answer1Button.setText(answers[0]);
         answer2Button.setText(answers[1]);
         answer3Button.setText(answers[2]);
         answer4Button.setText(answers[3]);
-        questionLabel.setFont(new Font("Serif", Font.BOLD, 28));
-        questionLabel.setForeground(new Color(255, 255, 255));
+        questionLabel.setFont(new Font("SansSerif", Font.BOLD, 28));
+        questionLabel.setForeground(Color.YELLOW);
         questionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(questionLabel, BorderLayout.CENTER);
-        answer1Button.setPreferredSize(new Dimension(160, 50));
-        answer2Button.setPreferredSize(new Dimension(160, 50));
-        answer3Button.setPreferredSize(new Dimension(160, 50));
-        answer4Button.setPreferredSize(new Dimension(160, 50));
+        add(questionLabel, BorderLayout.NORTH);
+        
         answer1Button.addActionListener(this::actionPerformed);
         answer2Button.addActionListener(this::actionPerformed);
         answer3Button.addActionListener(this::actionPerformed);
         answer4Button.addActionListener(this::actionPerformed);
-        answerPanel.setLayout(new FlowLayout());
+        answerPanel.setLayout(new GridLayout(2,2,4,4));
         answerPanel.add(answer1Button);
         answerPanel.add(answer2Button);
         answerPanel.add(answer3Button);
         answerPanel.add(answer4Button);
-        add(answerPanel, BorderLayout.PAGE_END);
+        setJButtonFont(answer1Button, font, color,Color.WHITE,dimenstion);
+        setJButtonFont(answer2Button, font, color,Color.WHITE,dimenstion);
+        setJButtonFont(answer3Button, font, color,Color.WHITE,dimenstion);
+        setJButtonFont(answer4Button, font, color,Color.WHITE,dimenstion);
+
+        add(answerPanel, BorderLayout.CENTER);
 
     }
 
@@ -72,5 +82,14 @@ public class QuestionPanel extends JPanel implements ActionListener {
         }
             Client.sendToServer(action);
         System.out.println(pickedAnswer);
+    }
+    
+    public void setJButtonFont(JButton button,Font font,Color color,Color color1,Dimension dimension){
+        button.setPreferredSize(dimension);
+        button.setFont(font);
+        button.setForeground(color1);
+        button.setBackground(color);
+        button.setBorderPainted(false);
+        button.setOpaque(true);
     }
 }
