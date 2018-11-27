ca7292418ec8d7b8f3ef108b9523f34c99f34c2d
William Jelvin <33911562+WJelvin@users.noreply.github.com>
2018-11-27 10:21:05 +0100

    added nameaction

---
+++ src/Sprint4_quizkamp_server/Server/Actions/NameAction.java
@@ -0,0 +1,11 @@
+package Sprint4_quizkamp_server.Server.Actions;
+
+import java.util.ArrayList;
+
+public class NameAction extends Action {
+    public String name;
+
+    public NameAction(String name) {
+        this.name = name;
+    }
+}
